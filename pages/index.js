9a57a3507929566e98b29e379570a831abf34510
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2024-06-18 00:44:10 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import A from "../components/A";
+import MainContainer from "../components/MainContainer";
+
+const Index = () => {
+  return (
+    <MainContainer keywords={"main page"}>
+      <h1>Main page</h1>
+    </MainContainer>
+  );
+};
+
+export default Index;
